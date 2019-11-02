356b5c77a7fb0a31d0e0abd22c517f15286c32a4
dishendra <dishendramishra@gmail.com>
2019-11-02 15:17:10 +0530

    added

---
+++ setup_proxy.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# =======================================================================
+#               Adding Commented Proxy
+# =======================================================================
+sudo su
+echo '#http_proxy="http://user:passwd@172.16.0.1:3128/"' >> /etc/environment
+echo '#https_proxy="http://user:passwd@172.16.0.1:3128/"' >> /etc/environment
+echo '#ftp_proxy="http://user:passwd@172.16.0.1:3128/"' >> /etc/environment
+echo '#HTTP_PROXY="http://user:passwd@172.16.0.1:3128/"' >> /etc/environment
+echo '#HTTPS_PROXY="http://user:passwd@172.16.0.1:3128/"' >> /etc/environment
+echo '#FTP_PROXY="http://user:passwd@172.16.0.1:3128/"' >> /etc/environment
+echo '#no_proxy=localhost,127.0.0.1,*.my.lan.domain' >> /etc/environment
+echo '#NO_PROXY=localhost,127.0.0.1,*.my.lan.domain' >> /etc/environment
+
+echo '#Acquire::http::proxy "http://user:passwd@172.16.0.1:3128";' >> /etc/apt/apt.conf.d/proxy.conf
+echo '#Acquire::https::proxy "http://user:passwd@172.16.0.1:3128";' >> /etc/apt/apt.conf.d/proxy.conf
+echo '#Acquire::ftp::proxy "http://user:passwd@172.16.0.1:3128";' >> /etc/apt/apt.conf.d/proxy.conf
+exit
+# =======================================================================
+
+
+# =======================================================================
+#               Adding proxy_on proxy_off functions
+# =======================================================================
+
+echo 'proxy_on(){' >> ~/.bashrc ~/.zshrc
+echo "	sudo sed -e '/h/ s/^#*//' -i /etc/environment /etc/apt/apt.conf.d/proxy.conf" >> ~/.bashrc ~/.zshrc
+echo '}' >> ~/.bashrc ~/.zshrc
+
+echo 'proxy_off(){' >> ~/.bashrc ~/.zshrc
+echo "	sudo sed -e '/h/ s/^#*/#/' -i /etc/environment /etc/apt/apt.conf.d/proxy.conf" >> ~/.bashrc ~/.zshrc
+echo '}' >> ~/.bashr ~/.zshrcc
+# =======================================================================
