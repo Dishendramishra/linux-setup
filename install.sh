71277dc9d700a1682dfc43066c5b0eb260f6ca76
Dishendra <dishendramishra@gmail.com>
2020-02-12 16:47:53 +0530

    added zsh-completitions

--- install.sh
+++ install.sh
@@ -84,11 +84,11 @@ sudo apt install -y albert
 echo -e ${RED} Installing Oh-My-Zsh ${Color_Off}
 sudo apt install -y zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
+exit
 # adding plugins
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
+git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
 
 # adding plugins in zshrc
 sed -i 's/plugins.*/plugins=(sudo git zsh-autosuggestions zsh-completions zsh-syntax-highlighting)/' ~/.zshrc
