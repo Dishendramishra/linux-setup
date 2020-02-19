33efe1672b72100eab87f2eedd0ef816081e38cc
dishendra <dishendragmail@gmail.com>
2020-02-19 14:26:33 +0530

    added auto root previliges

--- setup_proxy.sh
+++ setup_proxy.sh
@@ -6,13 +6,22 @@ GREEN='\033[1;32m'
 Color_Off="\033[0m"
 
 # =======================================================================
+#  getting root previliges
+# =======================================================================
+if [ $EUID != 0 ]; then
+    sudo "$0" "$@"
+    exit $?
+fi
+# =======================================================================
+
+# =======================================================================
 #               Adding Commented Proxy
 # =======================================================================
-echo -e ${GREEN}+----------------------------------------------------+
-echo "| Adding Commented Proxy to apt and /etc/environment |"
-echo -e '+----------------------------------------------------+' ${Color_Off}
+# echo -e ${GREEN}+----------------------------------------------------+
+echo -e ${GREEN}  Adding Commented Proxy to apt and /etc/environment ${Color_Off}
+# echo -e '+----------------------------------------------------+' ${Color_Off}
 
-sudo echo '\n'
+sudo echo '' >> /etc/environment
 sudo echo '#http_proxy="http://user:passwd@172.16.0.1:3128/"' >> /etc/environment
 sudo echo '#https_proxy="http://user:passwd@172.16.0.1:3128/"' >> /etc/environment
 sudo echo '#ftp_proxy="http://user:passwd@172.16.0.1:3128/"' >> /etc/environment
@@ -22,25 +31,27 @@ sudo echo '#FTP_PROXY="http://user:passwd@172.16.0.1:3128/"' >> /etc/environment
 sudo echo '#no_proxy=localhost,127.0.0.1,*.my.lan.domain' >> /etc/environment
 sudo echo '#NO_PROXY=localhost,127.0.0.1,*.my.lan.domain' >> /etc/environment
 
-sudo echo '\n'
+sudo echo '' >> /etc/apt/apt.conf.d/proxy.conf
 sudo echo '#Acquire::http::proxy "http://user:passwd@172.16.0.1:3128";' >> /etc/apt/apt.conf.d/proxy.conf
 sudo echo '#Acquire::https::proxy "http://user:passwd@172.16.0.1:3128";' >> /etc/apt/apt.conf.d/proxy.conf
 sudo echo '#Acquire::ftp::proxy "http://user:passwd@172.16.0.1:3128";' >> /etc/apt/apt.conf.d/proxy.conf
+echo -e ${BLUE} Done
+echo
 # =======================================================================
 
 
 # =======================================================================
 #               Adding proxy_on proxy_off functions
 # =======================================================================
-echo -e ${GREEN}'+-------------------------------------------+'
-echo "| Adding proxy_on proxy_off to bashrc zshrc |"
-echo -e '+-------------------------------------------+' ${Color_Off}
-echo '\n' >> ~/.bashrc
+# echo -e ${GREEN}'+-------------------------------------------+'
+echo -e ${GREEN}   Adding proxy_on proxy_off to bashrc zshrc ${Color_Off}
+# echo -e '+-------------------------------------------+' ${Color_Off}
+echo '' >> ~/.bashrc
 echo 'proxy_on(){' >> ~/.bashrc
 echo "	sudo sed -e '/h/ s/^#*//' -i /etc/environment /etc/apt/apt.conf.d/proxy.conf" >> ~/.bashrc
 echo '}' >> ~/.bashrc
 
-echo '\n' >> ~/.zshrc
+echo '' >> ~/.zshrc
 echo 'proxy_on(){' >> ~/.zshrc
 echo "	sudo sed -e '/h/ s/^#*//' -i /etc/environment /etc/apt/apt.conf.d/proxy.conf" >> ~/.zshrc
 echo '}' >> ~/.zshrc
@@ -52,4 +63,5 @@ echo '}' >> ~/.bashrc
 echo 'proxy_off(){' >> ~/.zshrc
 echo "	sudo sed -e '/h/ s/^#*/#/' -i /etc/environment /etc/apt/apt.conf.d/proxy.conf" >> ~/.zshrc
 echo '}' >> ~/.zshrc
+echo -e ${BLUE} Done
 # =======================================================================
