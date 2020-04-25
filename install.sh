75abbf65d50a8a7b9c7ab7a5162e7ded67c1be80
Dishendra <dishendramishra@gmail.com>
2020-04-26 00:43:28 +0530

    made script general for all DE

--- install.sh
+++ install.sh
@@ -10,30 +10,7 @@ sudo add-apt-repository universe
 sudo apt update && sudo apt upgrade -y
 
 echo -e ${RED} "Installing Basic Utilities" ${Color_Off}
-sudo apt  install -y git curl wget gparted apt-transport-https build-essential vlc htop easystroke intltool unrar-free unzip xdotool python3-dev python3-pip gnome-tweak-tool chrome-gnome-shell dconf-editor
-
-# installing some important python modules
-echo -e ${GREEN} Installing some important python modules ${Color_Off}
-pip3 install wheel
-pip3 install setuptools
-pip3 install jedi==0.14.1
-
-pip3 install scipy
-pip3 install astropy
-pip3 install matplotlib
-pip3 install pandas
-pip3 install julian
-
-pip3 install bs4
-pip3 install termcolor
-pip3 install colorama
-pip3 install requests
-
-pip3 install jupyter
-pip3 install notebook
-pip3 install ptipython
-pip3 install spyder
-
+sudo apt  install -y git curl wget gparted apt-transport-https build-essential vlc htop easystroke intltool unrar-free unzip xdotool dconf-editor
 
 # Google Chrome
 echo -e ${RED} adding Google chrome ${Color_Off}
@@ -55,10 +32,6 @@ sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode s
 echo -e ${RED} adding Sublime ${Color_Off}
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-
-# Papirus Icon theme
-echo -e ${RED} adding Papirus Icon theme ${Color_Off}
-sudo apt-add-repository -y ppa:papirus/papirus
 
 # Albert
 echo -e ${RED} adding Albert ${Color_Off}
@@ -91,19 +64,8 @@ sudo apt install -y code
 echo -e ${GREEN} Sublime Text ${Color_Off}
 sudo apt install -y sublime-text
 
-echo -e ${GREEN} Okular ${Color_Off}
-sudo apt install -y okular
-
-echo -e ${GREEN} Papirus Icon Theme ${Color_Off}
-sudo apt install -y papirus-icon-theme
-
 echo -e ${GREEN} Albert ${Color_Off}
 sudo apt install -y albert
-
-# echo -e ${GREEN} Free-Office ${Color_Off}
-# wget https://www.softmaker.net/down/softmaker-freeoffice-2018_973-01_amd64.deb
-# sudo dpkg -i ./softmaker-freeoffice-2018_973-01_amd64.deb
-# sudo /usr/share/freeoffice2018/add_apt_repo.sh
 # =======================================================================
 
 
